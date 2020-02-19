2ff382d3bd51822fb78436848b11a0d79666408d
Vinay <vrajk@apple.com>
2020-02-19 16:15:04 +0530

    Implemented CustomLoading

--- sample/sample/ViewController.swift
+++ sample/sample/ViewController.swift
@@ -10,22 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var loading : CustomLoader!
+//    var loading : CustomLoader!
+    
+    var loading : WebLoader!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        loading = CustomLoader(frame: CGRect.zero)
+        loading = WebLoader()
         view.addSubview(loading)
         addConstraints()
-        loading.backgroundColor = UIColor.cyan
-        loading.startAnimating()
+        loading.startAnimation()
+        
+//        loading = CustomLoader(frame: CGRect.zero)
+//        view.addSubview(loading)
+//        addConstraints()
+//        loading.backgroundColor = UIColor.cyan
+//        loading.startAnimating()
         // Do any additional setup after loading the view.
     }
     
     func addConstraints() {
         loading.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([loading.centerXAnchor.constraint(equalTo: view.centerXAnchor),loading.centerYAnchor.constraint(equalTo: view.centerYAnchor),loading.widthAnchor.constraint(equalToConstant: 100),loading.heightAnchor.constraint(equalToConstant: 100)])
+        NSLayoutConstraint.activate([loading.centerXAnchor.constraint(equalTo: view.centerXAnchor),loading.centerYAnchor.constraint(equalTo: view.centerYAnchor)])
         loading.layoutSubviews()
     }
 
